5d5834021840f0d4dffb66f80a90dbe026f79997
hau.nt <hau.nt@ou.edu.vn>
2024-03-26 16:30:41 +0700

    update api

--- ecourseapiv3/courses/admin.py
+++ ecourseapiv3/courses/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.html import mark_safe
 
-from courses.models import Course, Category
+from courses.models import Course, Category,Lesson, Comment, Tag, Like
 
 from django import forms
 from ckeditor_uploader.widgets import CKEditorUploadingWidget
@@ -29,3 +29,7 @@ class MyCourseAdmin(admin.ModelAdmin):
 
 admin.site.register(Category)
 admin.site.register(Course, MyCourseAdmin)
+admin.site.register(Lesson)
+admin.site.register(Comment)
+admin.site.register(Tag)
+admin.site.register(Like)
