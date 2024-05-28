7a5348802caff8f96732488180f425de8265a375
unino <phuongtrang06@gmail.com>
2024-05-28 13:45:12 +0700

    update login

--- ecoursemobilev4/App.js
+++ ecoursemobilev4/App.js
@@ -8,6 +8,10 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Register from "./components/users/Register";
 import Login from "./components/users/Login";
 import { Icon } from "react-native-paper";
+import { MyDispatchContext, MyUserContext } from "./configs/Context";
+import { useContext, useReducer } from "react";
+import MyUserReducer from "./configs/Reducers";
+import Profile from "./components/users/Profile";
 
 const Stack = createNativeStackNavigator();
 
@@ -23,19 +27,33 @@ const MyStack = () => {
 
 const Tab = createBottomTabNavigator();
 const MyTab = () => {
+  const user = useContext(MyUserContext);
+
   return (
     <Tab.Navigator>
       <Tab.Screen name="Home" component={MyStack} options={{title: "Danh mục khóa học", tabBarIcon: () => <Icon source="home" size={30} color="blue" />}} />
-      <Tab.Screen name="Register" component={Register} options={{title: "Đăng ký", tabBarIcon: () => <Icon source="account" size={30} color="blue" />}} />
-      <Tab.Screen name="Login" component={Login} options={{title: "Đăng nhập", tabBarIcon: () => <Icon source="login" size={30}  color="blue" />}} />
+
+      {user===null?<>
+        <Tab.Screen name="Register" component={Register} options={{title: "Đăng ký", tabBarIcon: () => <Icon source="account" size={30} color="blue" />}} />
+        <Tab.Screen name="Login" component={Login} options={{title: "Đăng nhập", tabBarIcon: () => <Icon source="login" size={30}  color="blue" />}} />
+      </>:<>
+        <Tab.Screen name="Profile" component={Profile} options={{title: user.username, tabBarIcon: () => <Icon source="login" size={30}  color="blue" />}} />
+      </>}
+      
     </Tab.Navigator>
   );
 }
 
 const App = () => {
+  const [user, dispatch] = useReducer(MyUserReducer, null);
+
   return (
     <NavigationContainer>
-      <MyTab />
+      <MyUserContext.Provider value={user}>
+        <MyDispatchContext.Provider value={dispatch}>
+          <MyTab />
+        </MyDispatchContext.Provider>
+      </MyUserContext.Provider>
     </NavigationContainer>
   );
 }
--- ecoursemobilev4/configs/APIs.js
+++ ecoursemobilev4/configs/APIs.js
@@ -1,15 +1,26 @@
 import axios from "axios";
 
 const BASE_URL = 'https://thanhduong.pythonanywhere.com/';
-// const BASE_URL = ' http://192.168.0.107:8000/';
 
 export const endpoints = {
     'categories': '/categories/',
     'courses': '/courses/',
     'lessons': (courseId) => `/courses/${courseId}/lessons/`,
     'lesson-detail': (lessonId) => `/lessons/${lessonId}/`,
-    'comments': (lessonId) => `/lessons/${lessonId}/comments/`
+    'comments': (lessonId) => `/lessons/${lessonId}/comments/`,
+    'register': '/users/',
+    'login': '/o/token/',
+    'current-user': '/users/current-user/'
 };
+
+export const authApi = (token) => {
+    return axios.create({
+        baseURL: BASE_URL,
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    });
+}
 
 export default axios.create({
     baseURL: BASE_URL
